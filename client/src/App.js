7ae919e9e9e94212e4512fc26e1e42b1392a1c45
hanjalicharis <thehhare@gmail.com>
2021-06-18 23:54:16 +0200

    started making changes, reading docs now

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+
+import hanjaries from './img/memories.jpeg';
 
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxwidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center"> Hanjary </Typography>
+                <img src={hanjaries} height="120" alt="hanjaries-logo" />
+            </AppBar>
+
+        </Container>
     );
 }
 
