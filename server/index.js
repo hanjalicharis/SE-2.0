c035a73f040689d708bfd0cbc51037edc9d2a109
hanjalicharis <thehhare@gmail.com>
2021-06-19 19:40:46 +0200

    changes

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js'
 
 const app = express();
+
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -14,8 +17,12 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://hanjalicharis:Gmailpro1@se-cluster.mu8zl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    res.send("Hello API");
+})
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
