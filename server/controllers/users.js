f01a1eafe3a0f8313929660171261c4b5be0f52b
hanjalicharis <thehhare@gmail.com>
2021-06-20 02:28:25 +0200

    register implemented

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -7,6 +7,25 @@ import users from '../models/user.js';
 
 export const signup = async (req, res) => {
 
+    const { email, password, firstName, lastName, confirmPassword } = req.body;
+
+    try {
+        const existingUser = await users.findOne({ email });
+        if (existingUser) return res.status(404).json({ message: 'Your account is already in our database. Please login!' })
+        if (password !== confirmPassword) return res.status(404).json({ message: 'Check your passwords!' })
+
+        const hash = await bcrypt.hash(password, 64);
+
+        const result = await users.create({ email, password: hash, name: firstName, surname: lastName });
+        const token = jwt.sign({ email: result.email, id: result._id }, 'hanjariesLoginToken', { expiresIn: "1h" });
+
+        res.status(200).json({ result, token });
+
+
+    } catch (error) {
+        res.status(500).json({ message: 'Something went wrong. Try again later!' })
+    }
+
 }
 
 export const signin = async (req, res) => {
